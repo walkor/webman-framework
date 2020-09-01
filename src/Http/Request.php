2e1b3aaff3c668b61de993338bc4a2543262470e
walkor <walkor@workerman.net>
2020-09-01 12:14:56 +0800

    add request->getRealIp()

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -142,6 +142,21 @@ class Request extends \Workerman\Protocols\Http\Request
     }
 
     /**
+     * @param bool $safe_mode
+     * @return string
+     */
+    public function getRealIp($safe_mode = true)
+    {
+        $remote_ip = $this->getRemoteIp();
+        if ($safe_mode && !static::isIntranetIp($remote_ip)) {
+            return $remote_ip;
+        }
+        return $this->header('client-ip', $this->header('x-forwarded-for',
+                   $this->header('x-real-ip', $this->header('x-client-ip',
+                   $this->header('via', $remote_ip)))));
+    }
+
+    /**
      * @return string
      */
     public function url()
@@ -189,4 +204,26 @@ class Request extends \Workerman\Protocols\Http\Request
         return false !== strpos($this->header('accept'), 'json');
     }
 
+    /**
+     * @param string $ip
+     * @return bool
+     */
+    public static function isIntranetIp($ip)
+    {
+        $reserved_ips = [
+            '167772160'  => 184549375,  /*    10.0.0.0 -  10.255.255.255 */
+            '3232235520' => 3232301055, /* 192.168.0.0 - 192.168.255.255 */
+            '2130706432' => 2147483647, /*   127.0.0.0 - 127.255.255.255 */
+            '2886729728' => 2887778303, /*  172.16.0.0 -  172.31.255.255 */
+        ];
+
+        $ip_long = ip2long($ip);
+
+        foreach ($reserved_ips as $ip_start => $ip_end) {
+            if (($ip_long >= $ip_start) && ($ip_long <= $ip_end)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
