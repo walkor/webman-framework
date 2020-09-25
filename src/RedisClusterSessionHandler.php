c228ced4d02f16caa2ebcbef4b69bf21a58f39de
walkor <walkor@users.noreply.github.com>
2020-09-25 17:09:12 +0800

    add prefix for RedisClusterSessionHandler

--- src/RedisClusterSessionHandler.php
+++ src/RedisClusterSessionHandler.php
@@ -20,13 +20,18 @@ class RedisClusterSessionHandler extends RedisSessionHandler
     {
         $this->_maxLifeTime = (int)ini_get('session.gc_maxlifetime');
         $timeout = $config['timeout'] ?? 2;
+        $read_timeout = $config['read_timeout'] ?? $timeout;
         $persistent = $config['persistent'] ?? false;
         $auth = $config['auth'] ?? '';
-        $args = [null, $config['host'], $timeout, $timeout, $persistent];
+        $args = [null, $config['host'], $timeout, $read_timeout, $persistent];
         if ($auth) {
             $args[] = $auth;
         }
         $this->_redis = new \RedisCluster(...$args);
+        if (empty($config['prefix'])) {
+            $config['prefix'] = 'redis_session_';
+        }
+        $this->_redis->setOption(\Redis::OPT_PREFIX, $config['prefix']);
     }
 
 }
