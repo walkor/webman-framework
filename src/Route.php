c2d673d7c2ca80f6e09a8db6e9cb326169bab5eb
walkor <walkor@users.noreply.github.com>
2020-12-07 17:15:01 +0800

    Update Route.php

--- src/Route.php
+++ src/Route.php
@@ -24,6 +24,11 @@ use Webman\App;
 class Route
 {
     /**
+     * @var Route[]
+     */
+    protected static $_instance = [];
+
+    /**
      * @var GroupCountBased
      */
     protected static $_dispatcher = null;
@@ -44,85 +49,136 @@ class Route
     protected static $_fallback = null;
 
     /**
+     * @var string
+     */
+    protected static $_groupPrefix = '';
+
+    /**
+     * @var Route[]
+     */
+    protected static $_nameList = [];
+
+    /**
+     * @var string|null
+     */
+    protected $_name = null;
+
+    /**
+     * @var array
+     */
+    protected $_methods = [];
+
+    /**
+     * @var string
+     */
+    protected $_path = '';
+
+    /**
+     * @var callable
+     */
+    protected $_callback = null;
+
+    /**
+     * Route constructor.
+     * @param $methods
+     * @param $path
+     */
+    public function __construct($methods, $path, $callback)
+    {
+        $this->_methods = (array) $methods;
+        $this->_path = $path;
+        $this->_callback = $callback;
+    }
+
+    /**
      * @param $path
      * @param $callback
+     * @return Route
      */
     public static function get($path, $callback)
     {
-        static::addRoute('GET', $path, $callback);
+        return static::addRoute('GET', $path, $callback);
     }
 
     /**
      * @param $path
      * @param $callback
+     * @return Route
      */
     public static function post($path, $callback)
     {
-        static::addRoute('POST', $path, $callback);
+        return static::addRoute('POST', $path, $callback);
     }
 
     /**
      * @param $path
      * @param $callback
+     * @return Route
      */
     public static function put($path, $callback)
     {
-        static::addRoute('PUT', $path, $callback);
+        return static::addRoute('PUT', $path, $callback);
     }
 
     /**
      * @param $path
      * @param $callback
+     * @return Route
      */
     public static function patch($path, $callback)
     {
-        static::addRoute('PATCH', $path, $callback);
+        return static::addRoute('PATCH', $path, $callback);
     }
 
     /**
      * @param $path
      * @param $callback
+     * @return Route
      */
     public static function delete($path, $callback)
     {
-        static::addRoute('DELETE', $path, $callback);
+        return static::addRoute('DELETE', $path, $callback);
     }
     
     /**
      * @param $path
      * @param $callback
+     * @return Route
      */
     public static function head($path, $callback)
     {
-        static::addRoute('HEAD', $path, $callback);
+        return static::addRoute('HEAD', $path, $callback);
     }
 
     /**
      * @param $path
      * @param $callback
+     * @return Route
      */
     public static function options($path, $callback)
     {
-        static::addRoute('OPTIONS', $path, $callback);
+        return static::addRoute('OPTIONS', $path, $callback);
     }
 
     /**
      * @param $path
      * @param $callback
+     * @return Route
      */
     public static function any($path, $callback)
     {
-        static::addRoute(['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'], $path, $callback);
+        return static::addRoute(['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'], $path, $callback);
     }
 
     /**
      * @param $method
      * @param $path
      * @param $callback
+     * @return Route
      */
     public static function add($method, $path, $callback)
     {
-        static::addRoute($method, $path, $callback);
+        return static::addRoute($method, $path, $callback);
     }
     
     /**
@@ -131,7 +187,78 @@ class Route
      */
     public static function group($path, $callback)
     {
+        static::$_groupPrefix = $path;
         static::$_collector->addGroup($path, $callback);
+        static::$_groupPrefix = '';
+    }
+
+    /**
+     * @return mixed|null
+     */
+    public function getName()
+    {
+        return $this->_name ?? null;
+    }
+
+    /**
+     * @param $name
+     * @return $this
+     */
+    public function name($name)
+    {
+        $this->_name = $name;
+        static::$_nameList[$name] = $this;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->_path;
+    }
+
+    /**
+     * @return array
+     */
+    public function getMethods()
+    {
+        return $this->_methods;
+    }
+
+    /**
+     * @return callable
+     */
+    public function getCallback()
+    {
+        return $this->_callback;
+    }
+
+    /**
+     * @param $name
+     * @return null|Route
+     */
+    public static function getByName($name)
+    {
+        return static::$_nameList[$name] ?? null;
+    }
+
+    /**
+     * @param $parameters
+     * @return string
+     */
+    public function url($parameters = [])
+    {
+        if (empty($parameters)) {
+            return $this->_path;
+        }
+        return preg_replace_callback('/\{(.*?)(?:\:[^\}]*?)*?\}/', function ($matches) use ($parameters) {
+            if (isset($parameters[$matches[1]])) {
+                return $parameters[$matches[1]];
+            }
+            return $matches[0];
+        }, $this->_path);
     }
 
     /**
@@ -172,13 +299,16 @@ class Route
      * @param $method
      * @param $path
      * @param $callback
+     * @return static
      */
     protected static function addRoute($method, $path, $callback)
     {
         static::$_hasRoute = true;
+        $route = new static($method, static::$_groupPrefix . $path, $callback);
         if ($callback = static::convertToCallable($path, $callback)) {
             static::$_collector->addRoute($method, $path, $callback);
         }
+        return $route;
     }
 
     /**
