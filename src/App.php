4395910bdb2a4234c4f910ac27392705c3bcd675
weiwei163@foxmail.com <ylqchengwei5566~>
2021-01-04 16:55:42 +0800

    Add: If fallback route is enabled, forced route matching is enabled.

--- src/App.php
+++ src/App.php
@@ -149,10 +149,11 @@ class App
                 return null;
             }
 
+            $fallback = Route::getFallback();
             $controller_and_action = static::parseControllerAction($path);
-            if (!$controller_and_action) {
+            if ($fallback || !$controller_and_action) {
                 // when route, controller and action not found, try to use Route::fallback
-                $callback = Route::getFallback() ?: function () {
+                $callback = $fallback ?: function () {
                     static $response_404;
                     if (!$response_404){
                         $response_404 = new Response(404, [], \file_get_contents(static::$_publicPath . '/404.html'));
