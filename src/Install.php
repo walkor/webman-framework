bf796dc236b842d859cbe61eca89dcb7bff0f4c8
walkor <walkor@users.noreply.github.com>
2022-03-11 23:17:38 +0800

    Add files via upload

--- src/Install.php
+++ src/Install.php
@@ -9,6 +9,7 @@ class Install
      * @var array
      */
     protected static $pathRelation = [
+        'start.php' => 'start.php',
         'support/helpers.php' => 'support/helpers.php',
         'support/bootstrap.php' => 'support/bootstrap.php',
         'support/Plugin.php' => 'support/Plugin.php',
