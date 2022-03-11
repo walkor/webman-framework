82d4099d88cff2233c88ac85aeac89583b110f15
walkor <walkor@users.noreply.github.com>
2022-03-11 21:17:22 +0800

    Update helpers.php

--- src/support/helpers.php
+++ src/support/helpers.php
@@ -31,7 +31,7 @@ if (is_phar()) {
 } else {
     define('BASE_PATH', realpath(__DIR__ . '/../'));
 }
-define('WEBMAN_VERSION', '1.2.5');
+define('WEBMAN_VERSION', '1.3.0');
 
 /**
  * @param $return_phar
