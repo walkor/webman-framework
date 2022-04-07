30ff495da9657e5c265ca3d3712c0d5590909469
walkor <walkor@users.noreply.github.com>
2022-04-07 12:04:39 +0800

    Update Request.php

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -251,15 +251,30 @@ class Request extends \Workerman\Protocols\Http\Request
      */
     public static function isIntranetIp($ip)
     {
+        // Not validate ip .
+        if (!filter_var($ip, FILTER_VALIDATE_IP)) {
+            return false;
+        }
+        // Is intranet ip ? For IPv4, the result of false may not be accurate, so we need to check it manually later .
+        if (!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)) {
+            return true;
+        }
+        // Manual check only for IPv4 .
+        if (!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+            return false;
+        }
+        // Manual check .
         $reserved_ips = [
-            '167772160'  => 184549375,  /*    10.0.0.0 -  10.255.255.255 */
-            '3232235520' => 3232301055, /* 192.168.0.0 - 192.168.255.255 */
-            '2130706432' => 2147483647, /*   127.0.0.0 - 127.255.255.255 */
-            '2886729728' => 2887778303, /*  172.16.0.0 -  172.31.255.255 */
+            1681915904 => 1686110207, // 100.64.0.0 -  100.127.255.255
+            3221225472 => 3221225727, // 192.0.0.0 - 192.0.0.255
+            3221225984 => 3221226239, // 192.0.2.0 - 192.0.2.255
+            3227017984 => 3227018239, // 192.88.99.0 - 192.88.99.255
+            3323068416 => 3323199487, // 198.18.0.0 - 198.19.255.255
+            3325256704 => 3325256959, // 198.51.100.0 - 198.51.100.255
+            3405803776 => 3405804031, // 203.0.113.0 - 203.0.113.255
+            3758096384 => 4026531839, // 224.0.0.0 - 239.255.255.255
         ];
-
         $ip_long = ip2long($ip);
-
         foreach ($reserved_ips as $ip_start => $ip_end) {
             if (($ip_long >= $ip_start) && ($ip_long <= $ip_end)) {
                 return true;
@@ -267,4 +282,5 @@ class Request extends \Workerman\Protocols\Http\Request
         }
         return false;
     }
+    
 }
