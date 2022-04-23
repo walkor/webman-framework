895088a1b24eab978b23578fd1e64d4b7ec1c0fc
walkor <walkor@users.noreply.github.com>
2022-04-23 20:07:48 +0800

    Update Route.php

--- src/Route.php
+++ src/Route.php
@@ -195,32 +195,33 @@ class Route
      */
     public static function resource(string $name, string $controller, array $options = [])
     {
-        if(is_array($options) && !empty($options)){
-            $diffOptions = array_diff($options,['index','create','store','update','show','edit','destroy','recovery']);
-            if(!empty($diffOptions)){
-                foreach ($diffOptions as $action) {
-                    static::any("/{$name}/{$action}[/{id}]", [$controller,$action])->name("{$name}.{$action}");
+        $name = trim($name, '/');
+        if (is_array($options) && !empty($options)) {
+            $diff_options = array_diff($options, ['index', 'create', 'store', 'update', 'show', 'edit', 'destroy', 'recovery']);
+            if (!empty($diff_options)) {
+                foreach ($diff_options as $action) {
+                    static::any("/{$name}/{$action}[/{id}]", [$controller, $action])->name("{$name}.{$action}");
                 }
             }
             // 注册路由 由于顺序不同会导致路由无效 因此不适用循环注册
-            if (in_array('index',$options)) static::get( "/{$name}", [$controller,'index'])->name("{$name}.index");
-            if (in_array('create',$options)) static::get( "/{$name}/create", [$controller,'create'])->name("{$name}.create");
-            if (in_array('store',$options)) static::post( "/{$name}", [$controller,'store'])->name("{$name}.store");
-            if (in_array('update',$options)) static::put("/{$name}/{id}", [$controller,'update'])->name("{$name}.update");
-            if (in_array('show',$options)) static::get( "/{$name}/{id}", [$controller,'show'])->name("{$name}.show");
-            if (in_array('edit',$options)) static::get( "/{$name}/{id}/edit", [$controller,'edit'])->name("{$name}.edit");
-            if (in_array('destroy',$options)) static::delete("/{$name}/{id}", [$controller,'destroy'])->name("{$name}.destroy");
-            if (in_array('recovery',$options)) static::put( "/{$name}/{id}/recovery", [$controller,'recovery'])->name("{$name}.recovery");
-        }else{
+            if (in_array('index', $options)) static::get("/{$name}", [$controller, 'index'])->name("{$name}.index");
+            if (in_array('create', $options)) static::get("/{$name}/create", [$controller, 'create'])->name("{$name}.create");
+            if (in_array('store', $options)) static::post("/{$name}", [$controller, 'store'])->name("{$name}.store");
+            if (in_array('update', $options)) static::put("/{$name}/{id}", [$controller, 'update'])->name("{$name}.update");
+            if (in_array('show', $options)) static::get("/{$name}/{id}", [$controller, 'show'])->name("{$name}.show");
+            if (in_array('edit', $options)) static::get("/{$name}/{id}/edit", [$controller, 'edit'])->name("{$name}.edit");
+            if (in_array('destroy', $options)) static::delete("/{$name}/{id}", [$controller, 'destroy'])->name("{$name}.destroy");
+            if (in_array('recovery', $options)) static::put("/{$name}/{id}/recovery", [$controller, 'recovery'])->name("{$name}.recovery");
+        } else {
             //为空时自动注册所有常用路由
-            if (method_exists($controller, 'index')) static::get("/{$name}", [$controller,'index'])->name("{$name}.index");
-            if (method_exists($controller, 'create')) static::get("/{$name}/create", [$controller,'create'])->name("{$name}.create");
-            if (method_exists($controller, 'store')) static::post("/{$name}", [$controller,'store'])->name("{$name}.store");
-            if (method_exists($controller, 'update')) static::put( "/{$name}/{id}", [$controller,'update'])->name("{$name}.update");
-            if (method_exists($controller, 'show')) static::get("/{$name}/{id}", [$controller,'show'])->name("{$name}.show");
-            if (method_exists($controller, 'edit')) static::get("/{$name}/{id}/edit", [$controller,'edit'])->name("{$name}.edit");
-            if (method_exists($controller, 'destroy')) static::delete( "/{$name}/{id}", [$controller,'destroy'])->name("{$name}.destroy");
-            if (method_exists($controller, 'recovery')) static::put("/{$name}/{id}/recovery", [$controller,'recovery'])->name("{$name}.recovery");
+            if (method_exists($controller, 'index')) static::get("/{$name}", [$controller, 'index'])->name("{$name}.index");
+            if (method_exists($controller, 'create')) static::get("/{$name}/create", [$controller, 'create'])->name("{$name}.create");
+            if (method_exists($controller, 'store')) static::post("/{$name}", [$controller, 'store'])->name("{$name}.store");
+            if (method_exists($controller, 'update')) static::put("/{$name}/{id}", [$controller, 'update'])->name("{$name}.update");
+            if (method_exists($controller, 'show')) static::get("/{$name}/{id}", [$controller, 'show'])->name("{$name}.show");
+            if (method_exists($controller, 'edit')) static::get("/{$name}/{id}/edit", [$controller, 'edit'])->name("{$name}.edit");
+            if (method_exists($controller, 'destroy')) static::delete("/{$name}/{id}", [$controller, 'destroy'])->name("{$name}.destroy");
+            if (method_exists($controller, 'recovery')) static::put("/{$name}/{id}/recovery", [$controller, 'recovery'])->name("{$name}.recovery");
         }
     }
 
