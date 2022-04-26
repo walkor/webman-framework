488e6842306a4ad11a87b83b751d5548553eb283
唐章明 <tangzhangming@live.com>
2022-04-26 11:06:14 +0800

    Update IdentityInterface.php

--- src/Auth/IdentityInterface.php
+++ src/Auth/IdentityInterface.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * This file is part of webman.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the MIT-LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author    tangzhangming <tangzhangming@live.com>
+ * @copyright tangzhangming <tangzhangming@live.com>
+ * @link      http://www.workerman.net/
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
 namespace Webman\Auth;
 
 interface IdentityInterface
