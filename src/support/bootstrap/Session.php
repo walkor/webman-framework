6a31794541ebd4d663cbdb9afc83ece19bc78278
walkor <walkor@users.noreply.github.com>
2022-05-15 22:40:19 +0800

    Session::$autoUpdateTimestamp

--- src/support/bootstrap/Session.php
+++ src/support/bootstrap/Session.php
@@ -35,21 +35,20 @@ class Session implements Bootstrap
         $config = config('session');
         Http::sessionName($config['session_name']);
         SessionBase::handlerClass($config['handler'], $config['config'][$config['type']]);
-        if (property_exists(SessionBase::class, 'lifetime')) {
-            $map = [
-                'cookie_lifetime' => 'cookieLifetime',
-                'gc_probability' => 'gcProbability',
-                'cookie_path' => 'cookiePath',
-                'lifetime' => 'lifetime',
-                'http_only' => 'httpOnly',
-                'domain' => 'domain',
-                'secure' => 'secure',
-                'same_site' => 'sameSite',
-            ];
-            foreach ($map as $key => $name) {
-                if (isset($config[$key])) {
-                    SessionBase::${$name} = $config[$key];
-                }
+        $map = [
+            'auto_update_timestamp' => 'autoUpdateTimestamp',
+            'cookie_lifetime' => 'cookieLifetime',
+            'gc_probability' => 'gcProbability',
+            'cookie_path' => 'cookiePath',
+            'http_only' => 'httpOnly',
+            'same_site' => 'sameSite',
+            'lifetime' => 'lifetime',
+            'domain' => 'domain',
+            'secure' => 'secure',
+        ];
+        foreach ($map as $key => $name) {
+            if (isset($config[$key]) && property_exists(SessionBase::class, $name)) {
+                SessionBase::${$name} = $config[$key];
             }
         }
     }
