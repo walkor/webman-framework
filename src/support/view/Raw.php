73772e700ba00691d5a8f9aa4019b0ffcd127fad
walkor <walkor@workerman.net>
2022-08-01 16:37:52 +0800

    support

---
+++ src/support/view/Raw.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * This file is part of webman.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the MIT-LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author    walkor<walkor@workerman.net>
+ * @copyright walkor<walkor@workerman.net>
+ * @link      http://www.workerman.net/
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace support\View;
+
+use Webman\View;
+use Throwable;
+
+/**
+ * Class Raw
+ * @package Support\View
+ */
+class Raw implements View
+{
+    /**
+     * @var array
+     */
+    protected static $_vars = [];
+
+    /**
+     * @param string $name
+     * @param mixed $value
+     */
+    public static function assign(string $name, $value = null)
+    {
+        static::$_vars = \array_merge(static::$_vars, \is_array($name) ? $name : [$name => $value]);
+    }
+
+    /**
+     * @param string $template
+     * @param array $vars
+     * @param string|null $app
+     * @return false|string
+     */
+    public static function render(string $template, array $vars, string $app = null)
+    {
+        $request = \request();
+        $plugin = $request->plugin ?? '';
+        $config_prefix = $plugin ? "plugin.$plugin." : '';
+        $view_suffix = \config("{$config_prefix}view.options.view_suffix", 'html');
+        $app = $app === null ? $request->app : $app;
+        $base_view_path = $plugin ? \base_path() . "/plugin/$plugin/app" : \app_path();
+        $view_path = $app === '' ? "$base_view_path/view/$template.$view_suffix" : "$base_view_path/$app/view/$template.$view_suffix";
+
+        \extract(static::$_vars, \EXTR_SKIP);
+        \extract($vars, \EXTR_SKIP);
+        \ob_start();
+        // Try to include php file.
+        try {
+            include $view_path;
+        } catch (Throwable $e) {
+            static::$_vars = [];
+            \ob_end_clean();
+            throw $e;
+        }
+        static::$_vars = [];
+        return \ob_get_clean();
+    }
+
+}
---
+++ src/support/view/Twig.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * This file is part of webman.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the MIT-LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author    walkor<walkor@workerman.net>
+ * @copyright walkor<walkor@workerman.net>
+ * @link      http://www.workerman.net/
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace support\View;
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+use Webman\View;
+
+/**
+ * Class Blade
+ * @package Support\View
+ */
+class Twig implements View
+{
+    /**
+     * @var array
+     */
+    protected static $_vars = [];
+
+    /**
+     * @param string $name
+     * @param mixed $value
+     */
+    public static function assign(string $name, $value = null)
+    {
+        static::$_vars = \array_merge(static::$_vars, \is_array($name) ? $name : [$name => $value]);
+    }
+
+    /**
+     * @param string $template
+     * @param array $vars
+     * @param string|null $app
+     * @return string
+     */
+    public static function render(string $template, array $vars, string $app = null)
+    {
+        static $views = [];
+        $request = \request();
+        $plugin = $request->plugin ?? '';
+        $app = $app === null ? $request->app : $app;
+        $config_prefix = $plugin ? "plugin.$plugin." : '';
+        $view_suffix = \config("{$config_prefix}view.options.view_suffix", 'html');
+        $key = "{$plugin}-{$request->app}";
+        if (!isset($views[$key])) {
+            $base_view_path = $plugin ? \base_path() . "/plugin/$plugin/app" : \app_path();
+            $view_path = $app === '' ? "$base_view_path/view/" : "$base_view_path/$app/view/";
+            $views[$key] = new Environment(new FilesystemLoader($view_path), \config("{$config_prefix}view.options", []));
+        }
+        $vars = \array_merge(static::$_vars, $vars);
+        $content = $views[$key]->render("$template.$view_suffix", $vars);
+        static::$_vars = [];
+        return $content;
+    }
+}
