d1a92b1caa62693c3867f0b535e0e4a780546983
ichynul <ichynul@163.com>
2022-09-03 18:50:56 +0800

    Update BusinessException.php

--- src/support/exception/BusinessException.php
+++ src/support/exception/BusinessException.php
@@ -26,6 +26,12 @@ class BusinessException extends Exception
 {
     public function render(Request $request): ?Response
     {
-        return null;
+        if ($request->expectsJson()) {
+            $code = $this->getCode();
+            $json = ['code' => $code ? $code : 500, 'msg' => $this->getMessage()];
+            return new Response(200, ['Content-Type' => 'application/json'],
+                \json_encode($json, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+        }
+        return new Response(200, [], $this->getMessage());
     }
 }
