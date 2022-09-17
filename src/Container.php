9e9430db27af3114d9d85ac82061af36e8aba172
kriss <462679766@qq.com>
2022-09-17 10:53:43 +0800

    Update Container.php
    support addDefinitions

--- src/Container.php
+++ src/Container.php
@@ -16,6 +16,10 @@ class Container implements ContainerInterface
      * @var array
      */
     protected $_instances = [];
+    /**
+     * @var array 
+     */
+    protected $_definitions = [];
 
     /**
      * @param string $name
@@ -25,10 +29,14 @@ class Container implements ContainerInterface
     public function get(string $name)
     {
         if (!isset($this->_instances[$name])) {
-            if (!\class_exists($name)) {
-                throw new NotFoundException("Class '$name' not found");
+            if (isset($this->_definitions[$name])) {
+                $this->_instances[$name] = call_user_func($this->_definitions[$name], $this);
+            } else {
+                if (!\class_exists($name)) {
+                    throw new NotFoundException("Class '$name' not found");
+                }
+                $this->_instances[$name] = new $name();
             }
-            $this->_instances[$name] = new $name();
         }
         return $this->_instances[$name];
     }
@@ -39,7 +47,8 @@ class Container implements ContainerInterface
      */
     public function has(string $name): bool
     {
-        return \array_key_exists($name, $this->_instances);
+        return \array_key_exists($name, $this->_instances)
+            || array_key_exists($name, $this->_definitions);
     }
 
     /**
@@ -54,6 +63,16 @@ class Container implements ContainerInterface
             throw new NotFoundException("Class '$name' not found");
         }
         return new $name(... array_values($constructor));
+    }
+
+    /**
+     * @param array $definitions
+     * @return $this
+     */
+    public function addDefinitions(array $definitions)
+    {
+        $this->_definitions = array_merge($this->_definitions, $definitions);
+        return $this;
     }
 
 }
