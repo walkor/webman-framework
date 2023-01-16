261fafa8d353dc9383abe4886645fdaf2fc44fd7
黎伟杰 <674416404@qq.com>
2023-01-16 14:35:22 +0800

    add laravel schema  support

---
+++ src/support/Schema.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This file is part of webman.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the MIT-LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author    walkor<walkor@workerman.net>
+ * @copyright walkor<walkor@workerman.net>
+ * @link      http://www.workerman.net/
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace support;
+
+class Schema
+{
+    /**
+     * @return \Illuminate\Database\Schema\Builder
+     */
+    public static function instance()
+    {
+        return Db::schema();
+    }
+
+    /**
+     * @param string $name
+     * @param array $arguments
+     * @return mixed
+     */
+    public static function __callStatic(string $name, array $arguments)
+    {
+        return static::instance()->{$name}(... $arguments);
+    }
+}
