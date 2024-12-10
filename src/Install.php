3bbc4728f600fb9f5360d2c889304fa2df1f818a
walkor <walkor@workerman.net>
2024-12-10 10:48:30 +0800

    Fix install

--- src/Install.php
+++ src/Install.php
@@ -39,15 +39,16 @@ class Install
      */
     public static function installByRelation()
     {
+        $basePath = realpath(__DIR__ . '/../../../../');
         foreach (static::$pathRelation as $source => $dest) {
             if ($pos = strrpos($dest, '/')) {
-                $parentDir = base_path() . '/' . substr($dest, 0, $pos);
+                $parentDir = $basePath . '/' . substr($dest, 0, $pos);
                 if (!is_dir($parentDir)) {
                     mkdir($parentDir, 0777, true);
                 }
             }
             $sourceFile = __DIR__ . "/$source";
-            copy_dir($sourceFile, base_path() . "/$dest", true);
+            copy_dir($sourceFile, $basePath . "/$dest", true);
             echo "Create $dest\r\n";
             if (is_file($sourceFile)) {
                 @unlink($sourceFile);
