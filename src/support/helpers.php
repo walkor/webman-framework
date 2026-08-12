4409510aaa72fdc306f72baaec7a2098f0babb12
KingBes <48203677+KingBes@users.noreply.github.com>
2026-08-12 10:26:30 +0800

    Update helpers.php

--- src/support/helpers.php
+++ src/support/helpers.php
@@ -235,6 +235,24 @@ if (!function_exists('redirect')) {
     }
 }
 
+if (!function_exists('assign')) {
+    /**
+     * assign function
+     *
+     * @param string|array $name
+     * @param mixed $value
+     * @param string|null $plugin
+     * @return void
+     */
+    function assign(string|array $name, mixed $value = null, ?string $plugin = null): void
+    {
+        $request = \request();
+        $plugin = $plugin === null ? ($request->plugin ?? '') : $plugin;
+        $handler = \config($plugin ? "plugin.$plugin.view.handler" : 'view.handler');
+        $handler::assign($name, $value);
+    }
+}
+
 if (!function_exists('view')) {
     /**
      * View response
